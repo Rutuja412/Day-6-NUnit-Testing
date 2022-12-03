4d6339980c5ae0525a4e76ab630eab6737d493c8
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-03 15:24:31 +0530

    Adding Temp Conv

---
+++ TempertureConversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TempertureConversion
+{
+    internal class Program
+    {
+        public class TempConv
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter value for Celcius to Fahrenheit conversion: ");
+                temperatureConversion.C = Convert.ToInt32(Console.ReadLine());
+                temperatureConversion.temperaturConversionToFahrenheit();
+
+                Console.WriteLine(" \n ");
+
+                Console.Write("Enter value for Fahrenheit to celsius conversion: ");
+                temperatureConversion.F = Convert.ToInt32(Console.ReadLine()); ;
+                temperatureConversion.temperaturConversionToCelsius();
+                Console.WriteLine(" \n ");
+            }
+        }
+    }
+}
